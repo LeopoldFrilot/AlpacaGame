a8d5a6799e9fdf4ff719e8e070008c8167b5a7df
LeopoldFrilot <LAFrilot@outlook.com>
2022-07-06 10:33:33 -0500

    Removing Unnecessary Firebase stuff

---
+++ Assets/Scripts/Statics/Helper.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper
+{
+    public static float MinutesToSeconds(float minutes)
+    {
+        return minutes * 60f;
+    }
+}
